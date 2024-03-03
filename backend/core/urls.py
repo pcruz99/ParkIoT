e5f97e1817edec2aef8fe182f93ebb127948b12b
Patricio Cruz <pcruz@cr11z-MacBook-Pro.local>
2024-03-03 01:19:44 -0500

    Add static files for REST framework and admin interface

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 # from api.authentication.viewsets.social_login import GithubSocialLogin
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path("api/users/", include(("api.routers", "api"), namespace="api")),
     path("parking/", include("parking.urls"), name="parking"),
     # path("api/sessions/oauth/github/", GithubSocialLogin.as_view(), name="github_login"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
